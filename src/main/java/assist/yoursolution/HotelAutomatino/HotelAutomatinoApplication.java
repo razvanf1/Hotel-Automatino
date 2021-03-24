d47c6603a8191728684806624e006977dd7c64db
razvanf1 <razvanfrunza42@gmail.com>
2021-03-24 16:15:07 +0200

    started blank project for test

---
+++ src/main/java/assist/yoursolution/HotelAutomatino/HotelAutomatinoApplication.java
@@ -0,0 +1,13 @@
+package assist.yoursolution.HotelAutomatino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAutomatinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAutomatinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assist/yoursolution/HotelAutomatino/HotelAutomatinoApplicationTests.java
@@ -0,0 +1,13 @@
+package assist.yoursolution.HotelAutomatino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAutomatinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
