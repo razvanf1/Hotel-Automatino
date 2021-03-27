43a24e0c33225e11242864b6f95b3e2c1311beac
razvanf1 <razvanfrunza42@gmail.com>
2021-03-27 11:31:08 +0200

    added lombok dependency

--- backend/src/main/java/yourSolution/springboot/model/Room.java
+++ backend/src/main/java/yourSolution/springboot/model/Room.java
@@ -1,76 +1,32 @@
 package yourSolution.springboot.model;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "rooms")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Room {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "room_status")
-    private int roomStatus;
+    @Column(name = "status")
+    private int status;
 
-    @Column(name = "room_type")
-    private int roomType;
+    @Column(name = "type")
+    private int type;
 
     @Column(name = "price")
     private int price;
 
-    @Column(name = "room_number")
-    private int roomNumber;
-
-    public Room() {
-
-    }
-
-    public Room(int roomStatus, int roomType, int price, int roomNumber) {
-        this.roomStatus = roomStatus;
-        this.roomType = roomType;
-        this.price = price;
-        this.roomNumber = roomNumber;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getRoomStatus() {
-        return roomStatus;
-    }
-
-    public void setRoomStatus(int roomStatus) {
-        this.roomStatus = roomStatus;
-    }
-
-    public int getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(int roomType) {
-        this.roomType = roomType;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(int roomNumber) {
-        this.roomNumber = roomNumber;
-    }
+    @Column(name = "number")
+    private int number;
 }
