f0a28c8edb16731e1817d5574b556f8d58a9da7c
marianpascari <constantin.pascari@student.usv.ro>
2021-03-29 18:31:15 +0300

    Update bd.sql

--- database/bd.sql
+++ database/bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2021 at 11:11 AM
+-- Generation Time: Mar 29, 2021 at 05:28 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `test`
+-- Database: `hotel_database`
 --
 
 -- --------------------------------------------------------
@@ -26,14 +26,13 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `admins`
 --
-CREATE DATABASE IF NOT EXISTS `test`;
-
 
 CREATE TABLE `admins` (
   `id` int(11) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp()
@@ -50,9 +49,37 @@ CREATE TABLE `guests` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `guest_id` int(11) NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `end_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations_rooms`
+--
+
+CREATE TABLE `reservations_rooms` (
+  `id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `reservation_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -65,19 +92,11 @@ CREATE TABLE `rooms` (
   `id` int(11) NOT NULL,
   `status` int(1) NOT NULL,
   `type` int(1) NOT NULL,
-  `price` int(10) NOT NULL,
+  `price` float NOT NULL,
   `number` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `rooms`
---
-
-INSERT INTO `rooms` (`id`, `status`, `type`, `price`, `number`, `created_at`, `updated_at`) VALUES
-(6, 1, 2, 160, 1, '2021-03-26 09:21:16', '2021-03-26 09:21:16'),
-(7, 0, 1, 120, 2, '2021-03-26 09:21:47', '2021-03-26 09:21:47');
 
 -- --------------------------------------------------------
 
@@ -90,6 +109,7 @@ CREATE TABLE `staff` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp()
@@ -110,6 +130,21 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `guests`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `guest_id` (`guest_id`);
+
+--
+-- Indexes for table `reservations_rooms`
+--
+ALTER TABLE `reservations_rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reservation_id` (`reservation_id`),
+  ADD KEY `room_id` (`room_id`);
 
 --
 -- Indexes for table `rooms`
@@ -141,6 +176,18 @@ ALTER TABLE `guests`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservations_rooms`
+--
+ALTER TABLE `reservations_rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
@@ -151,6 +198,23 @@ ALTER TABLE `rooms`
 --
 ALTER TABLE `staff`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`guest_id`) REFERENCES `guests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reservations_rooms`
+--
+ALTER TABLE `reservations_rooms`
+  ADD CONSTRAINT `reservations_rooms_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservations_rooms_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
